9ad67cd8fb12fff0d7679786bd0704f1c6ddeb87
Daria <92540217+dkk421@users.noreply.github.com>
2026-04-14 00:37:02 +0300

    Update main.py

--- Parsing_and_caching/main.py
+++ Parsing_and_caching/main.py
@@ -1,4 +1,3 @@
-# дан путь к файлу. (лог файлу). Над написать класс, в который в методе инит передается путь к папке реализующий различные метод с этими файлами.
 from dataclasses import dataclass
 from pathlib import Path
 import re
@@ -20,10 +19,8 @@ class LogReader:
         if not self._log_dir.is_dir():
             raise NotADirectoryError(f"Path is not a directory: {self._log_dir}")
 
-        # имя файла содержит инфу о дате и имя сервиса. Надо это извлечь {{service_name}}_{{date}}.log
         _FILE_RE = re.compile(r"^(?P_<service>.+)(?P<hour>\d{10})\.log$")
 
-        # основной класс должен знать имя лога, и его дату, чтобы выводить последние.
         def _parse_filename(self, path: Path) -> tuple[str, datetime]:
             match = self._FILE_RE.match(path.name)
 
