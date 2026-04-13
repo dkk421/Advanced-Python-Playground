14fba7ffbca42d78eca9eb7303b99f394ea11f2d
Daria <daria.kr421@gmail.com>
2026-04-14 00:27:46 +0300

    Add file processing example

---
+++ Parsing_and_caching/main.py
@@ -0,0 +1,39 @@
+# дан путь к файлу. (лог файлу). Над написать класс, в который в методе инит передается путь к папке реализующий различные метод с этими файлами.
+from dataclasses import dataclass
+from pathlib import Path
+import re
+
+
+@dataclass(frozen=True)
+class LogEvent:
+    service_name: str
+    event_type: str
+    datetime: datetime
+    message: str
+
+
+class LogReader:
+    def __init__(self, log_dir: str | Path) -> None:
+        self._log_dir = Path(log_dir)
+        if not self._log_dir.exists():
+            raise FileNotFoundError(f"Directory does not exist: {self._log_dir}")
+        if not self._log_dir.is_dir():
+            raise NotADirectoryError(f"Path is not a directory: {self._log_dir}")
+
+        # имя файла содержит инфу о дате и имя сервиса. Надо это извлечь {{service_name}}_{{date}}.log
+        _FILE_RE = re.compile(r"^(?P_<service>.+)(?P<hour>\d{10})\.log$")
+
+        # основной класс должен знать имя лога, и его дату, чтобы выводить последние.
+        def _parse_filename(self, path: Path) -> tuple[str, datetime]:
+            match = self._FILE_RE.match(path.name)
+
+            if match is None:
+                raise ValueError(f"Invalid log filename: {path.name}")
+            service_name = match.group("service")
+            hour_str = match.group("hour")
+
+            try:
+                hour_dt = datetime.strptime(hour_str, "%Y%m%d%H")
+            except ValueError as exc:
+                raise ValueError(f"Invalid date in filename: {path.name}") from exc
+            return service_name, hour_dt
